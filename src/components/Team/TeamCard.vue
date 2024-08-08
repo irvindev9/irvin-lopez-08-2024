461111cd58c16bbacaf9fae59937d44396f3e761
Irvin Raul Lopez <irvin.lopez@autofact.com.mx>
2024-08-08 15:09:42 -0600

    small detail in shadow

--- src/components/Team/TeamCard.vue
+++ src/components/Team/TeamCard.vue
@@ -52,8 +52,8 @@ function removePokemonFromTeam() {
 <template>
   <div class="card mb-3 shadow-sm">
     <div class="row g-0">
-      <div class="col-12 col-md-4 p-5">
-        <img :src="getImageUrl(pokemonNumber)" class="img-fluid rounded-start pokemon-img" alt="...">
+      <div class="col-12 col-md-4 p-5" v-if="pokemonInfo.types">
+        <img :src="getImageUrl(pokemonNumber)" class="img-fluid rounded-start pokemon-img" :class="`type-${pokemonInfo.types[0].type.name}`" alt="...">
       </div>
       <div class="col-12 col-md-8">
         <div class="card-body">
@@ -106,11 +106,82 @@ audio {
   height: 30px;
 }
 
-.pokemon-img:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.type-grass {
+  filter: drop-shadow(0 0 2em  rgba(155, 204, 80, 1));
+}
+.type-poison {
+  filter: drop-shadow(0 0 2em  rgba(191, 143, 202, 1));
 }
 
-.pokemon-img {
-  filter: drop-shadow(0 0 2em #000000aa);
+.type-fire {
+  filter: drop-shadow(0 0 2em  rgba(253, 125, 36, 1));
+}
+
+.type-flying {
+  filter: drop-shadow(0 0 2em  rgba(155, 204, 204, 1));
+}
+
+.type-water {
+  filter: drop-shadow(0 0 2em  rgba(69, 146, 196, 1));
+}
+
+.type-bug {
+  filter: drop-shadow(0 0 2em  rgba(168, 184, 32, 1));
+}
+
+.type-normal {
+  filter: drop-shadow(0 0 2em  rgba(168, 168, 120, 1));
+}
+
+.type-electric {
+  filter: drop-shadow(0 0 2em  rgba(251, 206, 53, 1));
+}
+
+.type-ground {
+  filter: drop-shadow(0 0 2em  rgba(219, 181, 77, 1));
+}
+
+.type-fairy {
+  filter: drop-shadow(0 0 2em  rgba(214, 133, 173, 1));
+}
+
+.type-fighting {
+  filter: drop-shadow(0 0 2em  rgba(213, 103, 97, 1));
+}
+
+.type-psychic {
+  filter: drop-shadow(0 0 2em  rgba(249, 85, 135, 1));
+}
+
+.type-rock {
+  filter: drop-shadow(0 0 2em  rgba(182, 161, 54, 1));
+}
+
+.type-steel {
+  filter: drop-shadow(0 0 2em  rgba(183, 183, 206, 1));
+}
+
+.type-ice {
+  filter: drop-shadow(0 0 2em  rgba(126, 206, 206, 1));
+}
+
+.type-ghost {
+  filter: drop-shadow(0 0 2em  rgba(123, 105, 188, 1));
+}
+
+.type-dragon {
+  filter: drop-shadow(0 0 2em  rgba(119, 108, 192, 1));
+}
+
+.type-dark {
+  filter: drop-shadow(0 0 2em  rgba(112, 88, 72, 1));
+}
+
+.type-unknown {
+  filter: drop-shadow(0 0 2em  rgba(0, 0, 0, 1));
+}
+
+.type-shadow {
+  filter: drop-shadow(0 0 2em  rgba(0, 0, 0, 1));
 }
 </style>
